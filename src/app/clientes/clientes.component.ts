ee9dc2b10f01e1fd54b545870a375a72810b3f93
Alex Boylan <aboylan@users.noreply.github.com>
2023-09-27 16:22:48 -0600

    Creando archivo clientes.json.ts con la lista de objetos

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,41 +8,10 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent {
 
-    clientes: Cliente[] = [
-      {
-        id: 1,
-        nombre: 'Alex',
-        apellido: 'Boylan',
-        email: 'alexb@bolsa.com',
-        createAt: '2023-09-27'
-      },
-      {
-        id: 2,
-        nombre: 'Mr John',
-        apellido: 'Doe',
-        email: 'john.doe@gmail.com',
-        createAt: '2023-09-28'
-      },
-      {
-        id: 3,
-        nombre: 'Linus',
-        apellido: 'Torvalds',
-        email: 'linus.torvalds@gmail.com',
-        createAt: '2023-09-29'
-      },
-      {
-        id: 4,
-        nombre: 'Rasmus',
-        apellido: 'Lerdorf',
-        email: 'rasmus.lerdorf@gmail.com',
-        createAt: '2023-09-30'
-      },
-      {
-        id: 5,
-        nombre: 'Erich',
-        apellido: 'Gamma',
-        email: 'erich.gamma@gmail.com',
-        createAt: '2023-09-30'
-      }
-    ];
+    clientes: Cliente[] = [];
+
+    ngOnInit() {
+      this.clientes = CLIENTES;
+    }
+
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,39 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {
+        id: 1,
+        nombre: 'Alex',
+        apellido: 'Boylan',
+        email: 'alexb@bolsa.com',
+        createAt: '2023-09-27'
+    },
+    {
+        id: 2,
+        nombre: 'Mr John',
+        apellido: 'Doe',
+        email: 'john.doe@gmail.com',
+        createAt: '2023-09-28'
+    },
+    {
+        id: 3,
+        nombre: 'Linus',
+        apellido: 'Torvalds',
+        email: 'linus.torvalds@gmail.com',
+        createAt: '2023-09-29'
+    },
+    {
+        id: 4,
+        nombre: 'Rasmus',
+        apellido: 'Lerdorf',
+        email: 'rasmus.lerdorf@gmail.com',
+        createAt: '2023-09-30'
+    },
+    {
+        id: 5,
+        nombre: 'Erich',
+        apellido: 'Gamma',
+        email: 'erich.gamma@gmail.com',
+        createAt: '2023-09-30'
+    }
+];
