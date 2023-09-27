9b0c0386425aa8b2aa1ea324b885113dfed2b003
Alex Boylan <aboylan@users.noreply.github.com>
2023-09-27 12:55:29 -0600

    Listando los objetos del tipo Cliente

--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -1,7 +1,17 @@
 export class Cliente {
+
     id: number;
     nombre: string;
     apellido: string;
     createAt: string;
     email: string;
+
+    constructor() { 
+        this.id = 0;
+        this.nombre = '';
+        this.apellido = '';
+        this.createAt = '';
+        this.email = '';
+    }
+
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  templateUrl: './clientes.component.html'
 })
 export class ClientesComponent {
 
+    clientes: Cliente[] = [
+      {
+        id: 1,
+        nombre: 'Alex',
+        apellido: 'Boylan',
+        email: 'alexb@bolsa.com',
+        createAt: '2023-09-27'
+      },
+      {
+        id: 2,
+        nombre: 'Mr John',
+        apellido: 'Doe',
+        email: 'john.doe@gmail.com',
+        createAt: '2023-09-28'
+      },
+      {
+        id: 3,
+        nombre: 'Linus',
+        apellido: 'Torvalds',
+        email: 'linus.torvalds@gmail.com',
+        createAt: '2023-09-29'
+      },
+      {
+        id: 4,
+        nombre: 'Rasmus',
+        apellido: 'Lerdorf',
+        email: 'rasmus.lerdorf@gmail.com',
+        createAt: '2023-09-30'
+      },
+      {
+        id: 5,
+        nombre: 'Erich',
+        apellido: 'Gamma',
+        email: 'erich.gamma@gmail.com',
+        createAt: '2023-09-30'
+      }
+    ];
 }
