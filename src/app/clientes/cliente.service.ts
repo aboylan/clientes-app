90da6c30db3847bea045c7970416fa0076c4adbf
Alex Boylan <aboylan@users.noreply.github.com>
2023-09-30 21:37:15 -0600

    Escribiendo implementacion crear en el cliente.service.ts y en form.component.ts

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-import { of, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { of, Observable, map } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Injectable({
@@ -11,12 +11,19 @@ import { HttpClient } from '@angular/common/http';
 export class ClienteService {
 
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  private httpHeaders: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
     //return of(CLIENTES);
-    return this.http.get<Cliente[]>(this.urlEndPoint);
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
+  }
+
+  create(cliente: Cliente) : Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders});
   }
 
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -10,9 +12,12 @@ export class FormComponent {
   public cliente: Cliente = new Cliente();
   public titulo: string = "Crear Cliente";
 
+  constructor(private clienteService: ClienteService, private router: Router) { }
+
   public create(): void {
-    console.log("Clicked!");
-    console.log(this.cliente);
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    );
   }
 
 }
